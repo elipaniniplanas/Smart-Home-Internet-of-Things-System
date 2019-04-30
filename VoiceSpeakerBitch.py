48c2dbc77fbff5a257ab6c07fe881c6b5dfb05cf
elipaniniplanas <43528404+elipaniniplanas@users.noreply.github.com>
2019-04-29 22:41:41 -0700

    Update and rename ircamera_sensor.py to VoiceSpeakerBitch.py

---
+++ VoiceSpeakerBitch.py
@@ -0,0 +1,4 @@
+# This code if for the voice activated speaker or virtual assistant
+# The psydo-idea of the code is to use the google voice assatant api to 
+# be able to translate microphone input ot python strings which can be 
+# instructions assigned to some actions.
